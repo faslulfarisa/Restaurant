b96f338daa25744cb773e5b4c3b830c763d56d8a
faslulfarisa <faslulfarisa5678@gmail.com>
2021-11-12 11:49:15 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const[count,setCount]=useState(0);
+  const[val,setVal]=useState(1);
+
+  
   return (
     <div className="App">
-     <h1>
-       Hello World
-     </h1>
+       <div className="count_button"
+       onClick={
+        ()=>{
+          setCount(count-1);
+        }
+      }>-</div>
+       <div className="count_value">{count}</div>
+     
+       <div className="count_button"
+       onClick={
+         ()=>{
+           setCount(count+1);
+         }
+       }>+</div>      
+
+       <div className="count_button"
+       onClick={
+        ()=>{
+          setVal(val*2);
+        }
+        }>*</div>
+       <div className="count_value">{val}</div>
+     
+       <div className="count_button"
+       onClick={
+         ()=>{
+           setVal(val/2);
+         }
+       }>/</div> 
+       <Heading number={2}/>     
     </div>
   );
 }
 
+const Heading = ({number})=>{
+  return(
+    <h1>Header {number} </h1>
+  )
+}
 export default App;
