11b6c1857ca01b7e0b95f689a5c89af41e61d9d1
faslulfarisa <faslulfarisa5678@gmail.com>
2021-11-29 10:47:47 +0530

    chess

--- src/CSS/Grid.jsx
+++ src/CSS/Grid.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import "./style.css"
+import pawnImage from "../assets/icons/chess-soldier.svg"
 
 const Grid = () => {
     return (
         <div className="grid-container">
             {[...Array(64)].map((_val,i)=>
-            {
+            {let indexArray=[i%8 ,Math.floor(i/8)];
+            console.log(indexArray)
+            
                 return(
                     <div style={{
-                        backgroundColor:i % 16 < 8 ? i%2? "black" :"white" : i%2 ? "white": "black",
-                        color:i%16 <8 ? i% 2 ? "white":"black" : i%2? "black": "white"
-                    }}></div>
+                        backgroundColor:(indexArray[1] + indexArray[0]) %2 ?  "#444" : "#ddd",
+                        color:(indexArray[1] + indexArray[0]) % 2 ? "#ddd": "#444",
+                    }}>
+                    {/* {indexArray[1]+","+indexArray[0]} */}
+                    {indexArray[1] === 1 && <img className="chess_piece" src ={pawnImage} alt="" />}
+                </div>
 
                 )
                
